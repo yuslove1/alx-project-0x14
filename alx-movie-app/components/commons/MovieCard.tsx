281f6a38f5cf29139c9fc9544bb487db5746cdc1
yuslove1 <adexyuslove@gmail.com>
2025-01-21 07:36:45 -0800

    chore: movie-app scaffolding

---
+++ alx-movie-app/components/commons/MovieCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MovieCard: React.FC = ({}) => {
+    return (
+        <div className="movie-card">
+        <p>movieCard content</p>
+        </div>
+    );
+};
+
+export default MovieCard;
---
+++ alx-movie-app/components/layouts/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+    return (
+        <footer>
+            <p>Footer content goes here</p>
+        </footer>
+    );
+};
+
+export default Footer;
